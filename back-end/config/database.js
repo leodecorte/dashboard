ebea9b5ed53c0edd14f484b33680cd8c714d4dd9
leodecorte <leodecorte@hotmail.com>
2022-03-31 15:50:01 +0200

    dashboard update

---
+++ back-end/config/database.js
@@ -0,0 +1,8 @@
+const config = {
+    user :'root',
+    password :'',
+    database:'dashboard',
+    port : 3306
+}
+
+module.exports = config; 
---
+++ back-end/db.sql
@@ -0,0 +1,12 @@
+
+CREATE DATABASE IF NOT EXISTS dashboard;
+
+USE dashboard;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
